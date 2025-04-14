65b8aa93b18ec41531a33dfe97f12433786035f2
Saddam Hussein <work.saddam786@gmail.com>
2025-04-14 22:59:46 +0530

    add multiple element using react

--- Chapter01-Inception/app.js
+++ Chapter01-Inception/app.js
@@ -6,16 +6,27 @@
 
 // Manipulate the HTML DOM using React
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "hello world! from react"
-);
-console.log(heading)     //In js-dom, it will gives the html element.
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hello world! from react"
+// );
+// console.log(heading)     //In js-dom, it will gives the html element.
                             //  But in react, it will gives an OBJECT. ✅
                             // React element is always an javascript object.
+
+const parent = React.createElement(    // It's the job of Core React to create an element.
+  "div",
+  { id: "parent" },
+  React.createElement("div", { id: "child" }, [
+    React.createElement("h1", {}, "i am h1 tag"),
+    React.createElement("h2", {}, "i am h2 tag"),
+  ])
+);
+console.log(parent)
 
 const root = ReactDOM.createRoot(document.getElementById("root")); //It's the job of ReactDOM to create root.
 // console.log(root)
 
-root.render(heading);
+// root.render(heading);
+root.render(parent);
