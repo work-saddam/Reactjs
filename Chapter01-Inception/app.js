bf92f4b5f751d531569d21e6f52fd45133371515
Saddam Hussein <work.saddam786@gmail.com>
2025-04-14 22:55:10 +0530

    Manipulate the HTML DOM using React

--- Chapter01-Inception/app.js
+++ Chapter01-Inception/app.js
@@ -1,5 +1,21 @@
 // Manipulate the HTML DOM using Javscript
-const heading = document.createElement("h1");
-heading.innerHTML = "Hello World!";
-const root = document.getElementById("root");
-root.appendChild(heading);
+// const heading = document.createElement("h1");
+// heading.innerHTML = "Hello World!";
+// const root = document.getElementById("root");
+// root.appendChild(heading);
+
+// Manipulate the HTML DOM using React
+
+const heading = React.createElement(
+  "h1",
+  { id: "heading" },
+  "hello world! from react"
+);
+console.log(heading)     //In js-dom, it will gives the html element.
+                            //  But in react, it will gives an OBJECT. ✅
+                            // React element is always an javascript object.
+
+const root = ReactDOM.createRoot(document.getElementById("root")); //It's the job of ReactDOM to create root.
+// console.log(root)
+
+root.render(heading);
