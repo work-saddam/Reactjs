073527cab11e711f763ac9cfb86a620090099b46
Saddam Hussein <work.saddam786@gmail.com>
2025-06-27 10:48:36 +0530

    add new url to fix cors issue

--- Chapter06-Exploring the world (useEffect)/Code/src/components/Body.js
+++ Chapter06-Exploring the world (useEffect)/Code/src/components/Body.js
@@ -26,7 +26,7 @@ const Body = () => {
 
   const fetchData = async () => {
     const data = await fetch(
-      "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
+      "https://food-reactjs-server.onrender.com/api/restaurants?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING"
     );
     const json = await data.json();
     // console.log(json?.data?.cards[4]?.card?.card?.gridElements?.infoWithStyle?.restaurants)
