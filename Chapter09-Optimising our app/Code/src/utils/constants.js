97f78ceee1aa0304b35223828439a873b3278aa6
Saddam Hussein <work.saddam786@gmail.com>
2025-06-28 22:11:55 +0530

    add new url to fix cors issue

--- Chapter09-Optimising our app/Code/src/utils/constants.js
+++ Chapter09-Optimising our app/Code/src/utils/constants.js
@@ -1,5 +1,5 @@
 export const SWIGGY_API =
-  "https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
+  "https://food-reactjs-server.onrender.com/api/restaurants?lat=12.9351929&lng=77.62448069999999&page_type=DESKTOP_WEB_LISTING";
 
 export const CDN_URL =
   "https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/";
@@ -8,4 +8,4 @@ export const LOGO_URL =
   "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS7qrUdpkljtKeYLOrV353LSEAuu7oNLrZyN35_fgDgxdh1ixbspLlhKPs&s";
 
 export const MENU_API =
-  "https://www.swiggy.com/dapi/menu/pl?page-type=REGULAR_MENU&complete-menu=true&lat=13.0365983&lng=80.2147026&restaurantId=";
+  "https://food-reactjs-server.onrender.com/api/menu?page-type=REGULAR_MENU&complete-menu=true&lat=13.0365983&lng=80.2147026&restaurantId=";
